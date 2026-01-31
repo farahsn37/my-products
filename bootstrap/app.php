78855094412fd79f4cc572395f164652172ab7be
farahsn37 <snfarah307@gmail.com>
2026-02-01 05:02:32 +0800

    add screenshot file

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,15 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        // $middleware->group('api', [
-        // 'throttle:api',
-        // \Illuminate\Routing\Middleware\SubstituteBindings::class,
-        // ]);
         $middleware->api(prepend: [
         \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
         ]);
-
-
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
